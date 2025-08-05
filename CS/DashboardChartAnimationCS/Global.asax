e7b6400c78faa166898692041014dc622771de18
Polina Tyureva <polina.tyureva@devexpress.com>
2025-08-05 18:12:29 +0400

    update example

---
+++ CS/DashboardChartAnimationCS/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DashboardChartAnimationCS.Global" Language="C#" %>
---
+++ CS/DashboardChartAnimationCS/Global.asax.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using DevExpress.Utils;
+
+namespace DashboardChartAnimationCS {
+    public class Global : System.Web.HttpApplication {
+
+        protected void Application_Start(object sender, EventArgs e) {
+            DeserializationSettings.RegisterTrustedClass(typeof(SalesPersonData));
+
+        }
+    }
+}
